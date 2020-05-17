4ab8b0e44dad79238a0dac1f9707389adeb5f6de
Matthew Gorton <mgorton@Matthews-MacBook-Pro.local>
2020-05-17 10:57:59 -0700

    exercise complete

--- exercise-3.py
+++ exercise-3.py
@@ -4,9 +4,12 @@ import sys
 
 if __name__ == '__main__':
 
-    input_string = input("pelase enter a string? ")
+    noun_string = input("pelase enter a noun? ")
+    verb_string = input("pelase enter a verb? ")
+    adjective_string = input("pelase enter a adjective? ")
+    adverb_string = input("pelase enter a adverb? ")
 
-    if len(input_string) == 0:
-        print("No string was entered")
-    else:
-        print(f"The string is {len(input_string) } characters long")
+    print()
+    print(f"Do you {verb_string} your {adjective_string} {noun_string} " +
+          f"{adverb_string}? That's Hillarious")
+    print()
